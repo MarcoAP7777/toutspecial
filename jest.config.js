98e2d5167dee8feb07673c3de1b75ca12fa7ff84
Marco <marco.pereira7777@hotmail.com>
2025-05-06 07:58:33 -0300

    feat: integra Codecov para relatórios de cobertura

--- jest.config.js
+++ jest.config.js
@@ -17,6 +17,16 @@ const customJestConfig = {
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/cypress/'],
   verbose: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "text", "clover"],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/pages/_app.tsx',
+    '!src/pages/_document.tsx',
+    '!**/node_modules/**',
+  ],
 };
 
 module.exports = createJestConfig(customJestConfig); 
